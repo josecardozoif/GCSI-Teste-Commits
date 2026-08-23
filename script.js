9ae82c57a4d6ac67aa919c49aa22fc29d09c64c0
José Eduardo Cardozo Araújo <jose.araujo6@estudante.ifms.edu.br>
2026-08-23 19:01:15 -0400

    feat: adding button script again

---
+++ script.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById("botao");
+const imagem = document.getElementById("image");
+
+botao.addEventListener("click", function() {
+  imagem.src = "images/image2.png";
+});
