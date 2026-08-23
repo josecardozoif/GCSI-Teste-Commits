95e38194b63618c8442b2ca9a7f7e280b02fbec3
José Eduardo Cardozo Araújo <jose.araujo6@estudante.ifms.edu.br>
2026-08-23 18:59:44 -0400

    remove: script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const botao = document.getElementById("botao");
-const imagem = document.getElementById("image");
-
-botao.addEventListener("click", function() {
-  imagem.src = "images/image2.png";
-});
